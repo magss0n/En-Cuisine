14b2b3a369f29e26183fc8aecce592c767a9f80b
Stephie-BTRSD <biloguistephie@gmail.com>
2025-05-03 07:02:43 +0100

    i have implemented the user authentication intentionally left test samples

---
+++ src/test/java/cm/sji/encuisine/Config/SimpleMVCConfig.java
@@ -0,0 +1,20 @@
+package cm.sji.encuisine.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+//public class SimpleMVCConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/").setViewName("index");
+//        registry.addViewController("/home").setViewName("index");
+//        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/dashboard").setViewName("dashboard");
+//        registry.addViewController("/admin/dashboard").setViewName("admin-dashboard");
+//        registry.addViewController("/public/page").setViewName("public-page");
+//    }
+//}
+
