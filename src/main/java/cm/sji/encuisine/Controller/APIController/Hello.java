3e586829ac567fa9d29a43e0d009fb9c520dad7c
Time_for_Business <moutcheuanderson@gmail.com>
2025-05-19 02:28:14 +0100

    Security configuration
    # Conflicts:
    #	src/main/resources/application.properties

---
+++ src/main/java/cm/sji/encuisine/Controller/APIController/Hello.java
@@ -0,0 +1,15 @@
+package cm.sji.encuisine.Controller.APIController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+
+}
